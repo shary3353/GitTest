eb73915d39ed2d8562f891300e4ca68aa1593abd
Parkjeonghun <shary3353@naver.com>
2021-03-11 11:56:47 +0900

    컨트롤러 추가, 무시사항 추가

---
+++ src/com/mvc/controller/MainController.java
@@ -0,0 +1,29 @@
+package com.mvc.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet({ "/main" })
+public class MainController extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		dual(req, resp);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		dual(req, resp);
+	}
+
+	private void dual(HttpServletRequest req, HttpServletResponse resp) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
